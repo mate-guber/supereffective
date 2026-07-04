1b061c576366903177230e8fbaa96541c7b42aa6
Mate Guber <45494572+mate-guber@users.noreply.github.com>
2026-07-04 15:25:28 +0200

    Implemented usage data fetching from smogon stats.

--- data/pipeline/meta_stats.py
+++ data/pipeline/meta_stats.py
@@ -1,16 +1,39 @@
 import httpx
+import datetime
 from sqlalchemy.orm import Session
 from app.models import Pokemon, MetaUsageData
 from data.pipeline.fetch_json import fetch_json
 
-usage_data_cache = {}
-pokemon_names = []
 
-def fetch_legal_pokemon_list() -> list[str]:
-    #Ffetch usage data from smogon stats.
+def fetch_usage_data(date: datetime.date, format_name: str) -> dict[str, float]:
+    """Fetches usage data from Smogon stats for a specific date and format.
+
+    Args:
+        date: A date object describing which year-month the data will be
+            pullef from. The day attribute does not matter.
+        format_name: Smogon stats' internal name representing different 
+            competitive formats or regulations.
+            eg.: gen9championsvgc2026regmb with -0/-1500/-1630/-1760 
+            attached to the end representing elo cutoff.
+    
+    Returns:
+        usage_data: A dict of pokemon names and their usage frequency.
+    """
+    # Fetch usage data from smogon stats.
     # Return a list of normalized pokemon names available in the format.
-    return pokemon_names
 
-def ingest_usage_data(session: Session, pokemon: list[Pokemon]) -> None:
+    with httpx.Client() as client:
+        stats = fetch_json(
+            client, 
+            f"https://www.smogon.com/stats/{date.year}-{date.month}/chaos/{format_name}.json"
+            )
+    
+    usage_data: dict[str, float] = {name: entry['usage'] for name, entry in stats['data'].items()}
+    return usage_data 
+
+def ingest_usage_data(session: Session, 
+                      pokemon: list[Pokemon], 
+                      usage_data: dict[str, float], 
+                      date: datetime.date) -> None:
     # Build MetaUsageData model objects then add them to the session
     pass
